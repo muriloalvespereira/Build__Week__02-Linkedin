f8f571700cc94e999ab6b97f1650ce882491599f
sars723 <sarasalomonn@gmail.com>
2021-07-19 14:37:50 +0200

    the horizontal line inside dashboard-bottom-container fixed

--- src/components-sara/Dashboard.jsx
+++ src/components-sara/Dashboard.jsx
@@ -46,8 +46,8 @@ const Dashboard = () => {
                 data-supported-dps="24x24"
                 fill="currentColor"
                 class="mercado-match"
-                width="32"
-                height="32"
+                width="24"
+                height="24"
                 focusable="false"
               >
                 <path d="M21 12h-1a9 9 0 00-9-9V2a10 10 0 0110 10zM11 5v1a6 6 0 016 6h1a7 7 0 00-7-7zm3 7h1a4 4 0 00-4-4v1a3 3 0 013 3zm-2 0a1 1 0 00-1.82-.54L5.32 6.6a8 8 0 00-.24 8.4 2.33 2.33 0 014.16.68l.88 3.08A8.57 8.57 0 0012 19a8 8 0 004.4-1.32l-4.86-4.86A1 1 0 0012 12zm-5 3a1.32 1.32 0 00-1.27 1L4 22h6l-1.73-6A1.32 1.32 0 007 15z"></path>
@@ -66,9 +66,9 @@ const Dashboard = () => {
             </div>{" "}
           </a>
         </div>
-        <div className="mynetwork">
+        <div className="mynetwork dashboard-section__hr">
           <a href="" className="">
-            <div className="mynetwork-content dashboard-section__cta">
+            <div className="mynetwork-content ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -88,9 +88,9 @@ const Dashboard = () => {
             </div>
           </a>
         </div>
-        <div className="salary-insights">
+        <div className="salary-insights dashboard-section__hr">
           <a href="">
-            <div className="salaryinsights-content dashboard-section__cta">
+            <div className="salaryinsights-content ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -109,9 +109,9 @@ const Dashboard = () => {
             </div>
           </a>
         </div>
-        <div className="myitems d-flex">
+        <div className="myitems d-flex dashboard-section__hr">
           <a href="">
-            <div className="myitems-content dashboard-section__cta">
+            <div className="myitems-content ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
