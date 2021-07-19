96e399bebd0657a67d45817bea697fe0faa3e675
Koulinn <drdverzola@gmail.com>
2021-07-19 07:52:34 +0100

    Initial Files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Styles/index.css';
+import App from './App';
+import './Styles/Member.css'
+import './Styles/Member2.css'
+import './Styles/Rafa.css'
+
+
+
+ReactDOM.render(
+
+    <App />,
+  
+  document.getElementById('root')
+);
+
+
