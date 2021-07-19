bb6cfcb132adb48e7f4028c44487efa61ca21a31
Koulinn <drdverzola@gmail.com>
2021-07-19 08:32:05 +0100

    Updated css files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Styles/index.css';
 import App from './App';
-import './Styles/Member.css'
-import './Styles/Member2.css'
+import './Styles/Hasham.css'
+import './Styles/Ingrid.css'
 import './Styles/Rafa.css'
+import './Styles/Murilo.css'
+import './Styles/Sara.css'
 
 
 
