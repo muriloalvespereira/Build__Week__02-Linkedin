75fc8ae87c3f928f8564164da8368b9b1ab03995
Koulinn <drdverzola@gmail.com>
2021-07-19 14:08:09 +0100

    Fixed NavBar height

--- src/Components/SearchInput.jsx
+++ src/Components/SearchInput.jsx
@@ -12,7 +12,7 @@ function SearchInput(props) {
             <InputGroup className="ml-4 border-0 flex-nowrap">
                 <InputGroup.Prepend>
                     <InputGroup.Text className="searchIcon border-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" dataSupportedDps="24x24" fill="currentColor" width="20" height="20" focusable="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" datasupporteddps="24x24" fill="currentColor" width="20" height="20" focusable="false">
                             <path d="M21.41 18.59l-5.27-5.28A6.83 6.83 0 0017 10a7 7 0 10-7 7 6.83 6.83 0 003.31-.86l5.28 5.27a2 2 0 002.82-2.82zM5 10a5 5 0 115 5 5 5 0 01-5-5z"></path>
                         </svg>                        </InputGroup.Text>
                 </InputGroup.Prepend>
@@ -22,11 +22,8 @@ function SearchInput(props) {
                     aria-describedby="inputGroup-sizing-default"
                     placeholder="Search"
                     onClick={handleShow}
-
                     onChange={(e) => console.log(e.target.value)}
                 />
-
-
 
             </InputGroup>
         </>
