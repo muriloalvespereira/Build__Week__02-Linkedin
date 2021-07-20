57bdfde1a438ebe3602dc38edbb9f29d695b574b
Koulinn <drdverzola@gmail.com>
2021-07-20 09:17:05 +0100

    Calling Post at Home Component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap"
 import LeftProfile from "./LeftProfile"
+import Post from './../components-sara/Post'
 
 const Home = () => {
     return (
@@ -8,7 +9,9 @@ const Home = () => {
                 <div className="px-0">
                     <LeftProfile />
                 </div>
-                <Col className="col-6"></Col>
+                <Col className="col-6">
+                    <Post></Post>
+                </Col>
                 <Col className="col-4"></Col>
             </Row>
         </Container>
