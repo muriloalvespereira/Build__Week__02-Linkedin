b077e3db0ee420bd905aa61cf8c03d88ef31f44a
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-07-20 08:11:38 +0100

    change picture to murilo

--- src/Components/NavBar/ProfileBox.jsx
+++ src/Components/NavBar/ProfileBox.jsx
@@ -6,15 +6,18 @@ const ProfileBox = () => (
     <ListGroup.Item className="ListGroupItem">
       <div className="image-colMod d-flex">
         <Image
-          src="https://media-exp1.licdn.com/dms/image/C5603AQF1wt8TGTy4Kw/profile-displayphoto-shrink_800_800/0/1624142581606?e=1632355200&v=beta&t=oIF_dMWsAz9_AldcLe4ytXcUwPz8AMLKGIomK6Y8d2A"
+          src="https://media-exp1.licdn.com/dms/image/C4D03AQErx2totdpx7w/profile-displayphoto-shrink_800_800/0/1613497757896?e=1632355200&v=beta&t=d7Rt8Arys31OsJVFU91XBSM-u2V0R9Mu3igTyN6Y-kM"
           width="48px"
           roundedCircle
           className="align-self-start"
         />
 
         <Col>
-          <span className="font-weight-bolder">Ingrid Oncken</span>
-          <p className="m-0">Web Development Student at Strive School</p>
+          <span className="font-weight-bolder">Murilo Alves</span>
+          <p className="m-0">
+            Web Development Student at Strive School, MSc Digital
+            Marketing/Sales Growth/Polyglot
+          </p>
         </Col>
       </div>
       <div className="d-grid mt-2 font-weight-bolder">
@@ -25,17 +28,29 @@ const ProfileBox = () => (
     </ListGroup.Item>
     <ListGroup.Item>
       <span className="font-weight-bolder">Account</span>
-      <Button variant="link" className="btn-linkMod">Settings & Privacy</Button>
-      <Button variant="link" className="btn-linkMod">Help</Button>
-      <Button variant="link" className="btn-linkMod">Language</Button>
+      <Button variant="link" className="btn-linkMod">
+        Settings & Privacy
+      </Button>
+      <Button variant="link" className="btn-linkMod">
+        Help
+      </Button>
+      <Button variant="link" className="btn-linkMod">
+        Language
+      </Button>
     </ListGroup.Item>
     <ListGroup.Item>
       <span className="font-weight-bolder">Manage</span>
-      <Button variant="link" className="btn-linkMod">Posts & Activity</Button>
-      <Button variant="link" className="btn-linkMod">Job Posting Account</Button>
+      <Button variant="link" className="btn-linkMod">
+        Posts & Activity
+      </Button>
+      <Button variant="link" className="btn-linkMod">
+        Job Posting Account
+      </Button>
     </ListGroup.Item>
     <ListGroup.Item>
-      <Button variant="link" className="btn-linkMod">Sign Out</Button>
+      <Button variant="link" className="btn-linkMod">
+        Sign Out
+      </Button>
     </ListGroup.Item>
   </ListGroup>
 )
