05f45fe4f34d099d5f615339bfba9a97ea189c95
hasham zafar <hasham307@gmail.com>
2021-07-20 09:13:17 +0200

    fixed sidebar

--- src/App.js
+++ src/App.js
@@ -1,57 +1,77 @@
 import './Styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { Container } from 'react-bootstrap';
+import Footer from './Components/Footer/Footer'
+import RightProfileCard from './Components/SideCards/RightProfileCard'
+import TopNavBar from './Components/NavBar/TopNavBar';
+import SearchOverlay from './Components/SearchOverlay';
+import Profile from './Components/Profile'
+import Dashboard from "./components-sara/Dashboard";
+
+
 
 const App = () => {
   const [loggedUser, setLoggedUser] = useState(false)
+  const [isSearchOn, setIsSearchOn] = useState(false)
 
-  useEffect(()=> {
+  useEffect(() => {
     getLoggedUser()
-   
   }, [])
 
   const getLoggedUser = async () => {
     try {
-        let response = await fetch('https://striveschool-api.herokuapp.com/api/profile/60f5264b0efe7800155c3494', {
-            method: 'Get',
-            headers: {
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGRjNWYwNmIzNTgxNzAwMTVjMjI3MDUiLCJpYXQiOjE2MjYyNzAyMjMsImV4cCI6MTYyNzQ3OTgyM30.0IcvG8-Zqf633mRWGCRlzG5yDVI6njZjZGZzJfuGulw",
-            }
-        })
-        let dataRequested = await response.json()
-        setLoggedUser(dataRequested)
+
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/profile/60f5264b0efe7800155c3494", {
+          method: "Get",
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGRjNWYwNmIzNTgxNzAwMTVjMjI3MDUiLCJpYXQiOjE2MjYyNzAyMjMsImV4cCI6MTYyNzQ3OTgyM30.0IcvG8-Zqf633mRWGCRlzG5yDVI6njZjZGZzJfuGulw",
+          },
+        }
+      )
+      let dataRequested = await response.json()
+      setLoggedUser(dataRequested)
     } catch (e) {
-        return e
+      return e
     }
-}
-  
+  }
+
   return (
-    
-       <> 
-       {loggedUser && <div className="container-fluid p-0 m-0">
-         {/* Row NavBar */}
-         <Container>
 
-         {/* Container Row > inside Profile Col8 sideBar col4 */}
-
-
-         </Container>
-       
+    <>
+      {loggedUser && 
+        <div className="container-fluid p-0 m-0">
+        <TopNavBar userInfo={loggedUser} setIsSearchOn={setIsSearchOn}></TopNavBar>
+        {isSearchOn && <SearchOverlay></SearchOverlay>}
+         
+          <Profile></Profile>
 
 
 
 
 
 
+        <Container>
+        <Footer></Footer>
+        </Container>
 
 
 
-         {console.log(loggedUser)}
-         </div>}
-       </>
-    
+
+
+
+
+
+
+
+        {console.log(loggedUser)}
+      </div>}
+    </>
+
   );
 }
 
-export default App;
+export default App
+
